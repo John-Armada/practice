280b908d6c7bc59ad05a04949234b80c58a859b7
Christ John Armada <cjs.armada@gmail.com>
2023-02-02 15:56:50 +0800

    Committing

---
+++ pycrash_course.py
@@ -0,0 +1,36 @@
+# print("Welcome to my Guessing Game!")
+# my_num = 14
+# guess = input("What number am I thinking? ")
+# while my_num != guess:
+#     if guess <= 8:
+#         print("That's too low")
+#     elif guess <= 13:
+#         print("Higher!")
+#     elif guess >= 18:
+#         print("Lower!")
+#     elif guess == 14:
+#         print("Gotcha!")
+#         break
+#     else:
+#         print("That's too high")
+
+# print("Welcome to my Guessing Game!")
+# my_num = 14
+# while True:
+#     guess = int(input("What number am I thinking? "))
+#     if guess < my_num:
+#         print("That's too low")
+#     elif guess > my_num:
+#         print("That's too high")
+#     else:
+#         print("Gotcha!")
+#         break
+
+# first_name, last_name = "Cj", "Armada"
+# full_name = first_name + " " + last_name
+# print(full_name)
+
+fav_num = 14
+message = f"My favorite number is {fav_num}"
+print(message)
+print("I am going to achieve my dreams in Life")

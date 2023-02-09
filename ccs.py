e233dfb79c73bbbf48cbdff8d184063a833f7238
Christ John Armada <cjs.armada@gmail.com>
2023-02-09 20:52:23 +0800

    Committing

---
+++ ccs.py
@@ -0,0 +1,37 @@
+#DONIE
+
+# A single node:
+#     contains data - integer, double or custom object
+#     - contains a reference pointing
+
+#Advantages
+#Linked lists are dynamic data
+
+
+
+
+#DISADVANTAGES
+#Waste memory becaas of the references
+#Nodes in a linked list must be read in order from the beginning as linked lists have sequential access (array items can be reached via indexes in )
+
+#1. Search
+
+# def is_palindrome(word):
+#     length = len(word)
+#     for i in range(length // 2):
+#         if word[i] != word[length - i - 1]:
+#             return False
+#     return True
+
+# word = input("Enter a word: ")
+# word = word.lower()
+
+# if is_palindrome(word):
+#     print("It's a palindrome.")
+# else:
+#     print("It's not a palindrome.")
+
+
+#EREGIA
+
+#Using Switch Statement
